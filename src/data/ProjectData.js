fa9a19b6c3d969884041ab7300d9653e2fd23e62
manny <115085160+manny-pro@users.noreply.github.com>
2023-01-11 03:18:43 +0100

    fix

---
+++ src/data/ProjectData.js
@@ -0,0 +1,101 @@
+// Project List
+export const ProjectList = [
+  {
+    img: 'https://raw.githubusercontent.com/manny-pro/portfolio-image/main/Capture%20d’écran%202023-01-11%20à%2001.34.30.png',
+    title: "My Portfolio",
+    description: "Bien sûr, j'ai créé mon propre site web personnel.",
+    tech_stack: "React, Javascript, Css",
+    github_url: "https://your_project_github_link",
+    demo_url: "https://your_project_demo_link",
+  },
+  {
+    img: 'https://raw.githubusercontent.com/manny-pro/portfolio-image/main/Uhmweather%20-%20iOS%20Weather%20App%20Template.jpg',
+    title: "Weather App",
+    description:
+      "C'est une simple application météo qui utilise l'API OpenWeatherMap pour obtenir les données météorologiques.",
+    tech_stack: "React-Native, Javascript, Css",
+    github_url: "https://github.com/manny-pro/weathers-app",
+  },
+  {
+    img: 'https://raw.githubusercontent.com/manny-pro/portfolio-image/main/Capture%20d’écran%202023-01-10%20à%2016.33.43.png',
+    title: "E-commerce",
+    description: "Création d'un site de commerce électronique simple en utilisant reactjs.",
+    tech_stack: "Reactjs, Css",
+    github_url: "https://your_project_github_link",
+    demo_url: "https://your_project_demo_link/",
+  },
+  {
+    img: 'https://raw.githubusercontent.com/manny-pro/portfolio-image/main/Capture%20d’écran%202023-01-10%20à%2016.36.05.png',
+    title: "Gagnant du Code Camp ETNA",
+    description:
+      "J'ai participé au code camp organisé par l'ETNA qui consistait à dynamiser leurs intranet et j'ai gagné le premier prix.",
+    tech_stack: "MongoDB, Javascript, Reactjs, Css",
+    github_url: "https://github.com/manny-pro/defi-etna",
+  },
+];
+
+// --------------------------------------- Skills
+
+export const stackList = [
+  {
+    img: "https://assets.stickpng.com/images/5847f5bdcef1014c0b5e489c.png",
+    name: "HTML",
+  },
+  {
+    img: "https://www.seekpng.com/png/full/141-1415372_css3-icon-png.png",
+    name: "CSS",
+  },
+  {
+    img: "https://upload.wikimedia.org/wikipedia/commons/6/6a/JavaScript-logo.png",
+    name: "JavaScript",
+  },
+  {
+    img: "https://icons.veryicon.com/png/o/business/vscode-program-item-icon/typescript-def.png",
+    name: "TypeScript",
+  },
+  {
+    img: "https://www.pngplay.com/wp-content/uploads/5/Alphabet-C-PNG-HD-Quality.png",
+    name: "C",
+  },
+  {
+    img: "https://assets.stickpng.com/images/5848152fcef1014c0b5e4967.png",
+    name: "Python",
+  },
+  {
+    img: "https://assets.stickpng.com/images/584830f5cef1014c0b5e4aa1.png",
+    name: "ReactJS",
+  },
+  {
+    img: "https://git-scm.com/images/logos/downloads/Git-Icon-1788C.png",
+    name: "Git",
+  },
+  {
+    img: "https://upload.wikimedia.org/wikipedia/commons/thumb/7/7f/Responsive_Web_Design_Logo.svg/1200px-Responsive_Web_Design_Logo.svg.png",
+    name: "Responsive",
+  },
+  {
+    img: "https://www.pngkey.com/png/full/128-1286315_bird-logo-vector-2-buy-clip-art-swift.png",
+    name: "Swift",
+  },
+  {
+    img: "https://seeklogo.com/images/N/nodejs-logo-FBE122E377-seeklogo.com.png",
+    name: "NodeJS",
+  },
+  {
+    img: "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9a/Visual_Studio_Code_1.35_icon.svg/2048px-Visual_Studio_Code_1.35_icon.svg.png",
+    name: "VS Code",
+  },
+  {
+    img: "https://www.pngall.com/wp-content/uploads/13/Mongodb-PNG-Image-HD.png",
+    name: "MongoDB",
+  },
+  {
+    img: "https://assets.stickpng.com/images/5848104fcef1014c0b5e4950.png",
+    name: "MySQL",
+  },
+  {
+    img: "https://upload.wikimedia.org/wikipedia/commons/thumb/2/22/MacOS_logo_%282017%29.svg/2060px-MacOS_logo_%282017%29.svg.png",
+    name: "Mac OS",
+  },
+];
+
